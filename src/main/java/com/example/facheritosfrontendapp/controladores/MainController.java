75fb5b379c9989f3234d2285bbcf4b763990a07b
andresfeliper3 <andresfeliper.2001@gmail.com>
2022-05-29 10:41:50 -0500

    preparación para hacer el dashboard

--- src/main/java/com/example/facheritosfrontendapp/controladores/MainController.java
+++ src/main/java/com/example/facheritosfrontendapp/controladores/MainController.java
@@ -1,14 +1,38 @@
 package com.example.facheritosfrontendapp.controladores;
 
+import com.example.facheritosfrontendapp.vistas.Main;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class MainController {
+    private Stage stage;
+    private Scene scene;
     @FXML
     private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
+    private Button button;
+    @FXML
+    protected void onHelloButtonClick(ActionEvent event) throws IOException {
         welcomeText.setText("Welcome to JavaFX Application!");
+        switchToDashBoard(event);
     }
+
+    protected void switchToDashBoard(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("dashboard-view.fxml"));
+        stage = (Stage)((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setScene(scene);
+        stage.show();
+    }
+
 }
