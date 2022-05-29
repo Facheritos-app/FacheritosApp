3eaeabfd5adf152e417e49c1f2e13cdb53e4b9b2
sebastian6551 <sebastian6551@live.com>
2022-05-29 16:21:32 -0500

    Welcome manager scene

--- src/main/java/com/example/facheritosfrontendapp/vistas/Main.java
+++ src/main/java/com/example/facheritosfrontendapp/vistas/Main.java
@@ -5,16 +5,18 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+
 import java.io.IOException;
 
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        System.out.println(Main.class.getResource("hello-view.fxml"));
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
+        System.out.println(Main.class.getResource("welcomeManager.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("welcomeManager.fxml"));
 
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        //Scene Welcome Manager
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Welcome");
         stage.setScene(scene);
         stage.show();
     }
