8e7608a69be261328f013b3895331949576f545c
andresfeliper3 <andresfeliper.2001@gmail.com>
2022-05-29 09:17:08 -0500

    estructura de archivos

--- src/main/java/com/example/facheritosfrontendapp/controladores/MainController.java
+++ src/main/java/com/example/facheritosfrontendapp/controladores/MainController.java
@@ -1,9 +1,9 @@
-package com.example.facheritosfrontendapp;
+package com.example.facheritosfrontendapp.controladores;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class MainController {
     @FXML
     private Label welcomeText;
 
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,12 @@ module com.example.facheritosfrontendapp {
     requires javafx.fxml;
 
 
-    opens com.example.facheritosfrontendapp to javafx.fxml;
-    exports com.example.facheritosfrontendapp;
+    //opens com.example.facheritosfrontendapp to javafx.fxml;
+    //exports com.example.facheritosfrontendapp;
+
+    exports com.example.facheritosfrontendapp.vistas;
+    opens com.example.facheritosfrontendapp.vistas to javafx.fxml;
+    exports com.example.facheritosfrontendapp.controladores;
+    opens com.example.facheritosfrontendapp.controladores to javafx.fxml;
+
 }
