18ebc1f30008ced099e82ed9b2cc69613841de6b
andresfeliper3 <andresfeliper.2001@gmail.com>
2022-05-29 23:36:55 -0500

    página de bienvenida, faltan iconos

--- src/main/java/com/example/facheritosfrontendapp/vistas/Main.java
+++ src/main/java/com/example/facheritosfrontendapp/vistas/Main.java
@@ -9,14 +9,18 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class Main extends Application {
+
     @Override
     public void start(Stage stage) throws IOException {
         System.out.println(Main.class.getResource("welcomeManager.fxml"));
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("welcomeManager.fxml"));
 
         //Scene Welcome Manager
+
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Welcome");
+        String css = this.getClass().getResource("style.css").toExternalForm();
+        scene.getStylesheets().add(css);
         stage.setScene(scene);
         stage.show();
     }
