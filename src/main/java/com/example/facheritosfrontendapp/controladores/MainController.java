e6a8b225f0b9acaac7ac5718a2417ce623d67d6f
JuanR <juan.randazzo@correounivalle.edu.co>
2022-06-01 10:45:35 -0500

    Conexion del endpoint Login con backend (Basico)

--- src/main/java/com/example/facheritosfrontendapp/controladores/MainController.java
+++ src/main/java/com/example/facheritosfrontendapp/controladores/MainController.java
@@ -1,17 +1,21 @@
 package com.example.facheritosfrontendapp.controladores;
 
+import com.example.facheritosfrontendapp.dto.LoginDTO;
+import com.example.facheritosfrontendapp.dto.WorkerDTO;
+import com.example.facheritosfrontendapp.endpoints.LoginEndpoint;
 import com.example.facheritosfrontendapp.vistas.Main;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+
+import com.google.gson.Gson;
 
 public class MainController {
 
@@ -25,9 +29,27 @@ public class MainController {
     @FXML
     private Button button;
 
+    private LoginEndpoint loginEndpoint = new LoginEndpoint();
+
+
     @FXML
     protected void boton(ActionEvent event) throws IOException {
         switchToDashBoard(event);
+        LoginDTO loginDTO = new LoginDTO();
+
+        loginDTO.setCc("1234567");
+        loginDTO.setPassword("Admin123+");
+
+        Gson gson = new Gson();
+
+        String jsonObject = gson.toJson(loginDTO);
+
+        try {
+            WorkerDTO responseWorker = loginEndpoint.sendCredentials(jsonObject);
+            System.out.println(responseWorker.getFirst_name());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     } ;
 
     protected void switchToDashBoard(ActionEvent event) throws IOException {
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,11 @@
 module com.example.facheritosfrontendapp {
     requires javafx.controls;
     requires javafx.fxml;
+    requires okhttp3;
+    requires annotations;
+    requires okio;
+    requires lombok;
+    requires com.google.gson;
 
 
     //opens com.example.facheritosfrontendapp to javafx.fxml;
@@ -10,5 +15,6 @@ module com.example.facheritosfrontendapp {
     opens com.example.facheritosfrontendapp.vistas to javafx.fxml;
     exports com.example.facheritosfrontendapp.controladores;
     opens com.example.facheritosfrontendapp.controladores to javafx.fxml;
+    opens com.example.facheritosfrontendapp.dto to com.google.gson;
 
 }
