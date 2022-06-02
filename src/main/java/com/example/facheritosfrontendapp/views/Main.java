5994d0aca271ee6653244ba238cb455a790c1720
jennyferbm13 <jennyfer.belalcazar@correounivalle.edu.co>
2022-06-02 11:29:56 -0500

    Login

--- src/main/java/com/example/facheritosfrontendapp/views/Main.java
+++ src/main/java/com/example/facheritosfrontendapp/views/Main.java
@@ -5,7 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
-
+import javafx.scene.image.Image;
 
 import java.io.IOException;
 
@@ -18,9 +18,9 @@ public class Main extends Application {
 
 
         //Scene Welcome Manager
-
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Welcome");
+        //stage.getIcons().add( new Image("https://www.purina-latam.com/sites/g/files/auxxlc391/files/styles/social_share_large/public/01_%C2%BFQu%C3%A9-puedo-hacer-si-mi-gato-est%C3%A1-triste-.png?itok=w67Nhubc"));
+        //stage.getIcons().add(new Image(this.getClass().getResource("welcome-manager.png").toExternalForm()));
         String css = this.getClass().getResource("style.css").toExternalForm();
         scene.getStylesheets().add(css);
         stage.setScene(scene);
