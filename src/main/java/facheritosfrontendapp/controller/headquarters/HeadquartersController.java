d35f0b48d44f93a633d5f637d128c79ccfc54a1f
andresfeliper3 <andresfeliper.2001@gmail.com>
2022-06-05 19:11:18 -0500

    primera versión de agregar sedes

--- src/main/java/facheritosfrontendapp/controller/headquarters/HeadquartersController.java
+++ src/main/java/facheritosfrontendapp/controller/headquarters/HeadquartersController.java
@@ -1,5 +1,29 @@
 package facheritosfrontendapp.controller.headquarters;
 
-public class HeadquartersController {
+import facheritosfrontendapp.controller.DashboardController;
+import facheritosfrontendapp.controller.MainController;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HeadquartersController implements Initializable {
+
+    @FXML
+    private Button addHeadquarter;
+
+    private DashboardController dashboardController;
+
+    @FXML
+    protected void addHeadquarterClicked() throws IOException {
+        dashboardController.navbarClicked("headquarters/headquartersAdd");
+    }
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        dashboardController = MainController.getDashboardController();
+    }
 
 }
