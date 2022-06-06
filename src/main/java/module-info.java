7bbea6d62ac921e3b85c017936c61b4b6252150f
andresfeliper3 <andresfeliper.2001@gmail.com>
2022-06-06 15:06:16 -0500

    boton de añadir usuarios

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -18,10 +18,14 @@ module  facheritosfrontendapp {
     opens  facheritosfrontendapp.controller to javafx.fxml;
     exports  facheritosfrontendapp.controller.navbar;
     opens  facheritosfrontendapp.controller.navbar to javafx.fxml;
+    exports  facheritosfrontendapp.controller.users;
+    opens  facheritosfrontendapp.controller.users to javafx.fxml;
     exports  facheritosfrontendapp.controller.headquarters;
     opens  facheritosfrontendapp.controller.headquarters to javafx.fxml;
     opens  facheritosfrontendapp.dto.loginDTO to com.google.gson;
     opens  facheritosfrontendapp.dto.personDTO to com.google.gson;
     opens  facheritosfrontendapp.dto.otherDTO to com.google.gson;
+    //exports facheritosfrontendapp.controller.users;
+    //opens facheritosfrontendapp.controller.users to javafx.fxml;
 
 }
