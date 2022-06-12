1438e54b48078488851377213495d232502385cf
andresfeliper3 <andresfeliper.2001@gmail.com>
2022-06-12 17:26:35 -0500

    creacion de MyDialogPane

---
+++ src/main/java/facheritosfrontendapp/views/ConfirmationCancelController.java
@@ -0,0 +1,25 @@
+package facheritosfrontendapp.views;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.DialogPane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ConfirmationCancelController  implements Initializable {
+
+    @FXML
+    private DialogPane dialogPane;
+
+    private Button yesBtn;
+    private Button noBtn;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
