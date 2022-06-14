4904f04039316a7005ede824fd2422ae5b37841a
JuanR <juan.randazzo@correounivalle.edu.co>
2022-06-14 09:12:58 -0500

    Implemento llamadas a la BD en ver usuario unico, tambien comento

--- src/main/java/facheritosfrontendapp/controller/user/UserSingleViewController.java
+++ src/main/java/facheritosfrontendapp/controller/user/UserSingleViewController.java
@@ -1,6 +1,8 @@
 package facheritosfrontendapp.controller.user;
 
+import backend.endpoints.headquarterEndpoint.HeadquarterEndpoint;
 import backend.endpoints.workerEndpoint.WorkerEndpoint;
+import facheritosfrontendapp.ComboBoxView.HeadquarterView;
 import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
@@ -11,8 +13,9 @@ import javafx.scene.control.*;
 import java.net.URL;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Map;
+import java.util.Optional;
 import java.util.ResourceBundle;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
@@ -20,6 +23,9 @@ import java.util.concurrent.ExecutionException;
 public class UserSingleViewController implements Initializable {
 
     private WorkerEndpoint workerEndpoint;
+    private HeadquarterEndpoint headquarterEndpoint;
+
+    private ArrayList<HeadquarterView> headquarterComboboxList;
 
     //Here are all the @FXML components
     @FXML
@@ -72,11 +78,14 @@ public class UserSingleViewController implements Initializable {
 
 
     @FXML
-    /*
-    editAction: event -> void
-    Purpose: By pressing the 'Editar usuario' button the text fields are enabled,
-    the title becomes visible and the 'Guardar' and 'Cancelar' buttons are enabled.
-    * */
+
+
+
+    /**
+     * editAction: event -> void
+     * Purpose: By pressing the 'Editar usuario' button the text fields are enabled,
+     * the title becomes visible and the 'Guardar' and 'Cancelar' buttons are enabled.
+     */
     protected void editAction(ActionEvent event) {
         editUserLabel.setVisible(true);
 
@@ -101,8 +110,14 @@ public class UserSingleViewController implements Initializable {
 
     public UserSingleViewController() {
         workerEndpoint = new WorkerEndpoint();
+        headquarterEndpoint = new HeadquarterEndpoint();
+        headquarterComboboxList = new ArrayList<>();
     }
 
+    /**
+     * showForm: Integer -> void
+     * Purpose: This method contains all the other methods that together make showing the form possible
+     */
     public void showForm(Integer idPerson) {
         new Thread(() -> {
             CompletableFuture<Map<Boolean, ResultSet>> userCall = CompletableFuture.supplyAsync(() -> workerEndpoint.getWorkerById(idPerson));
@@ -110,6 +125,13 @@ public class UserSingleViewController implements Initializable {
                 userCall.thenApply((response) -> {
                     if (response.containsKey(true)) {
                         ResultSet resultSet = response.get(true);
+                        try {
+                            setHeadquarterCombo();
+                        } catch (ExecutionException e) {
+                            throw new RuntimeException(e);
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
                         Platform.runLater(() -> {
                             try {
                                 setForm(resultSet);
@@ -129,23 +151,64 @@ public class UserSingleViewController implements Initializable {
 
     }
 
+
+    public void setHeadquarterCombo() throws ExecutionException, InterruptedException {
+        CompletableFuture<Map<Boolean, ResultSet>> headquarterCall = CompletableFuture.supplyAsync(() -> headquarterEndpoint.getHeadquarters());
+
+        headquarterCall.thenApply((response) -> {
+            if(response.containsKey(true)){
+                ResultSet resultSet = response.get(true);
+                try {
+                    fillHeadquarterCombo(resultSet);
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return true;
+        }).get();
+    }
+
+    public void fillHeadquarterCombo(ResultSet resultSet) throws SQLException {
+        while(resultSet.next()){
+            Integer idHeadquarter = resultSet.getInt("id_headquarter");
+            String name = resultSet.getString("name");
+            headquarterComboboxList.add(new HeadquarterView(idHeadquarter, name));
+        }
+        headquarterCombo.setItems(FXCollections.observableArrayList(headquarterComboboxList));
+    }
+
     public void setForm(ResultSet resultSet) throws SQLException {
         nameField.setText(resultSet.getString("first_name"));
         lastNameField.setText(resultSet.getString("last_name"));
-        roleCombo.getSelectionModel().select(resultSet.getInt("id_type_person"));
-        headquarterCombo.getSelectionModel().select(resultSet.getString("name"));
+        roleCombo.getSelectionModel().select(resultSet.getInt("id_type_person") - 1);
+        headquarterCombo.getSelectionModel().select(findHeadquarterById(resultSet.getInt("id_headquarter")).toString());
         phoneField.setText(resultSet.getString("cellphone"));
         emailField.setText(resultSet.getString("email"));
         birthdayPicker.setValue(resultSet.getDate("birthday").toLocalDate());
         idField.setText(resultSet.getString("cc"));
-        statusCombo.getSelectionModel().select(String.valueOf(resultSet.getBoolean("state")));
+        statusCombo.getSelectionModel().select(resultSet.getBoolean("state") ? 0 : 1); //Ternary if to convert boolean into integer.
         salaryField.setText(String.valueOf(resultSet.getDouble("salary")));
         cityCombo.getSelectionModel().select(resultSet.getString("city_name"));
+    }
+
+    /**
+     * findHeadquarterById: Integer -> HeadquarterView
+     * Purpose: This method finds a headquarter by its id.
+     * It is used to set the headquarter combobox given the id of the headquarter where the worker is settled.
+     */
+    public HeadquarterView findHeadquarterById(Integer id){
+        for(Integer i = 0; i < headquarterComboboxList.size(); i++){
+            if(id == headquarterComboboxList.get(i).getIdHeadquarter()){
+                return headquarterComboboxList.get(i);
+            }
+        }
+        return new HeadquarterView(-100,"");
     }
 
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        statusCombo.setItems(FXCollections.observableArrayList("Activo", "Inactivo"));
         roleCombo.setItems(FXCollections.observableArrayList("Gerente", "Vendedor", "Jefe de taller"));
     }
 }
