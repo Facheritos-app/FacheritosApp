d0e9095f3e82c5c1eb68ef8039771006e00779bb
JuanR <juan.randazzo@correounivalle.edu.co>
2022-06-27 10:02:21 -0500

    Implemento ver inventario en ver detalles de cotizacion

--- src/main/java/facheritosfrontendapp/controller/quotation/QuotationSingleViewController.java
+++ src/main/java/facheritosfrontendapp/controller/quotation/QuotationSingleViewController.java
@@ -1,5 +1,6 @@
 package facheritosfrontendapp.controller.quotation;
 
+import backend.endpoints.inventoryEndpoint.InventoryEndpoint;
 import backend.endpoints.quotationEndpoint.QuotationEndpoint;
 import facheritosfrontendapp.objectRowView.inventoryRowView.VehicleRowView;
 import javafx.application.Platform;
@@ -8,6 +9,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseEvent;
 
 import java.math.BigDecimal;
 import java.net.URL;
@@ -23,8 +25,9 @@ public class QuotationSingleViewController implements Initializable {
 
 
     private QuotationEndpoint quotationEndpoint;
-
-    private ArrayList<VehicleRowView> vehicleRowViewList;
+    private InventoryEndpoint inventoryEndpoint;
+    private ArrayList<VehicleRowView> vehicleQuotationRowList;
+    private ArrayList<VehicleRowView> vehicleInventoryRowList;
 
     @FXML
     private TableView quotationTableView;
@@ -34,6 +37,16 @@ public class QuotationSingleViewController implements Initializable {
     private TableColumn<VehicleRowView, Double> colPrice;
     @FXML
     private TableColumn<VehicleRowView, String> colHeadquarter;
+    @FXML
+    private TableView inventoryTableView;
+    @FXML
+    private TableColumn<VehicleRowView, String> colNameInventory;
+    @FXML
+    private TableColumn<VehicleRowView, Double> colPriceInventory;
+    @FXML
+    private TableColumn<VehicleRowView, String> colHeadquarterInventory;
+    @FXML
+    private TableColumn<VehicleRowView, Integer> colQuantityInventory;
 
     @FXML
     private TextField sellerCc;
@@ -60,9 +73,14 @@ public class QuotationSingleViewController implements Initializable {
     private Label quotationPrice;
     @FXML
     private Label quotationId;
+    @FXML
+    private Button cancel;
+
     public QuotationSingleViewController(){
         quotationEndpoint = new QuotationEndpoint();
-        vehicleRowViewList = new ArrayList<>();
+        inventoryEndpoint = new InventoryEndpoint();
+        vehicleQuotationRowList = new ArrayList<>();
+        vehicleInventoryRowList = new ArrayList<>();
     }
 
     public void showQuotation(Integer idQuotation){
@@ -94,6 +112,54 @@ public class QuotationSingleViewController implements Initializable {
         }).start();
     }
 
+    public void showInventory(){
+        new Thread(() -> {
+            CompletableFuture<Map<Boolean, ResultSet>> vehiclesCall = CompletableFuture.supplyAsync(() -> inventoryEndpoint.getVehiclesForTableView());
+
+            try {
+                vehiclesCall.thenApply(response -> {
+                    if(response.containsKey(true)){
+                        ResultSet resultSet = response.get(true);
+                        try {
+                            setVehiclesData(resultSet);
+                        } catch (SQLException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                    return true;
+                }).get();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            } catch (ExecutionException e) {
+                throw new RuntimeException(e);
+            }
+        }).start();
+    }
+
+    public void setVehiclesData(ResultSet resultSet) throws SQLException {
+        while(resultSet.next()){
+            VehicleRowView vehicleRow = new VehicleRowView(resultSet.getString("description"), new BigDecimal(String.valueOf(resultSet.getDouble("price"))).toPlainString(),
+                                                            resultSet.getString("name"), resultSet.getInt("quantity"),
+                                                            resultSet.getInt("id_car"));
+            vehicleInventoryRowList.add(vehicleRow);
+        }
+
+        colNameInventory.setCellValueFactory(new PropertyValueFactory<>("name"));
+        colPriceInventory.setCellValueFactory(new PropertyValueFactory<>("price"));
+        colHeadquarterInventory.setCellValueFactory(new PropertyValueFactory<>("headquarter"));
+        colQuantityInventory.setCellValueFactory(new PropertyValueFactory<>("quantity"));
+
+        inventoryTableView.setItems(FXCollections.observableArrayList(vehicleInventoryRowList));
+    }
+
+    @FXML
+    public void editQuotation(MouseEvent mouseEvent){
+        showInventory();
+        sellerCc.setDisable(false);
+        customerCc.setDisable(false);
+        paymentMethod.setDisable(false);
+    }
+
     public void setSellerInfo(ResultSet resultSet) throws SQLException {
         sellerCc.setText(resultSet.getString("seller_cc"));
         sellerName.setText(resultSet.getString("seller_firstname"));
@@ -118,13 +184,13 @@ public class QuotationSingleViewController implements Initializable {
     public void setQuotationItems(ResultSet resultSet) throws SQLException {
         VehicleRowView vehicleRowView = new VehicleRowView(resultSet.getString("description"), new BigDecimal(String.valueOf(resultSet.getDouble("price"))).toPlainString(),
                 resultSet.getString("name"), 1, resultSet.getInt("id_car"));
-        vehicleRowViewList.add(vehicleRowView);
+        vehicleQuotationRowList.add(vehicleRowView);
 
         colName.setCellValueFactory(new PropertyValueFactory<>("name"));
         colPrice.setCellValueFactory(new PropertyValueFactory<>("price"));
         colHeadquarter.setCellValueFactory(new PropertyValueFactory<>("headquarter"));
 
-        quotationTableView.setItems(FXCollections.observableArrayList(vehicleRowViewList));
+        quotationTableView.setItems(FXCollections.observableArrayList(vehicleQuotationRowList));
     }
 
 
