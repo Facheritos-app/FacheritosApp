c35205e94514699aa74439ebc05e8ae2e5b59217
JuanCamilo-R <juan.randazzo@correounivalle.edu.co>
2022-07-12 16:50:37 -0500

    Cambio plantilla de reportes

--- src/main/java/facheritosfrontendapp/controller/report/ReportController.java
+++ src/main/java/facheritosfrontendapp/controller/report/ReportController.java
@@ -9,12 +9,11 @@ public class ReportController {
     JasperConnection jasperConnection;
 
     public ReportController(){
-        jasperConnection = new JasperConnection("FacheritosReports/sellersReport.jrxml");
+        jasperConnection = new JasperConnection("FacheritosReports/sales/sellersReport.jrxml");
     }
 
     @FXML
     public void sellersClicked(MouseEvent mouseEvent) {
-        System.out.println("");
         jasperConnection.showReport();
     }
 }
